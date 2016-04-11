4e5a1c7def372b368820ffae66fcf01a75d1a16f
annaimpson <annaimpson@gmail.com>
2016-04-11 10:57:31 -0400

    search component added

--- app/node/app.js
+++ app/node/app.js
@@ -16,22 +16,31 @@ app.use(cors()); //allows overriding cross origin policy (use npm install if nee
 //    });
 // });
 //p is the query param for pagination
-app.get('/breweries', function(req, res){ // listens for request on /api route
-  var lat = req.query.lat; // grabs lat and lng queries from the request object
-  var lng = req.query.lng;
+app.get('/breweries', function(req, res){ // listens for request on /breweries route
   var p;
   if(req.query.p){
     p = req.query.p
   }else{
     p = 1;
   }
-
   request('http://api.brewerydb.com/v2/breweries/?key=9b561e70ba317f8d99aaa277053fe0fd&p=' + p + '&name=*', function (error, response, body) { // api url
     if (!error && response.statusCode === 200) {
       res.send(body); // if no errors, send the body of data back to front end
     }else{
       console.log(error);
       res.send(response);
+    }
+   });
+});
+
+
+app.get('/search', function(req, res){ // listens for request on /search route
+  var type = req.query.type;
+  var withLocations = req.query.withLocations;
+  request('https://api.brewerydb.com/v2/search/?key=9b561e70ba317f8d99aaa277053fe0fd&type=' + type + '&withLocations=Y' + withLocations, function (error, response, body) { // api url
+    if (!error && response.statusCode === 200) {
+      console.log('beer');
+      res.send(body); // if no errors, send the body of data back to front end
     }
    });
 });
